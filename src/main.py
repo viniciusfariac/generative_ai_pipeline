1ac76efc65ef24b5dd6f8ae371ed3e14c2128231
viniciusfariac <vfariacerqueira@gmail.com>
2025-12-26 21:34:20 -0300

    feat: messages from ia to users

--- src/main.py
+++ src/main.py
@@ -3,11 +3,11 @@ from transform import generate_news
 from load import load_users
 import pandas as pd
 
+
 def main():
     path = "data/users.csv"
     df = extract_users(path)
-
-    print(df.head())
+    generate_news(df, None)
 
 
 if __name__ == "__main__":
--- src/transform.py
+++ src/transform.py
@@ -1,2 +1,25 @@
-def generate_news(df, output_path):
-    pass
+from dotenv import load_dotenv
+import os
+import groq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+load_dotenv()
+
+def generate_news(df):
+    groq_api_key = os.getenv("GROQ_API_KEY")
+    
+    for index, cliente in df.iterrows():
+
+        prompt_template = f"Voce é uma ia do marketing Santander, especializada em enviar mensagens para clientes do banco dizendo como investimento é bom. Essa é as informações do usuario: {cliente["Nome"]}, {cliente["Conta"]}"
+
+        modelo = ChatGroq(
+        groq_api_key=groq_api_key,
+        model="openai/gpt-oss-20b",
+        temperature=0.1
+        )
+
+        resposta = modelo.invoke(prompt_template)
+        print(resposta.content)
+
+    
+    
